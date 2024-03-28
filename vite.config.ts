f4d29c6b22729812bc22e74abdd331c7698187fb
Thijmen Tang <thijmtang2003@gmail.com>
2024-03-28 16:43:55 +0100

    Fix
    default vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,72 +1,7 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig, loadEnv } from 'vite';
-import plugin from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-
-import dotenv from 'dotenv'
-import viteReact from '@vitejs/plugin-react';
-
-dotenv.config()
-
-const baseFolder =
-    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
-        ? `${process.env.APPDATA}/ASP.NET/https`
-        : `${process.env.HOME}/.aspnet/https`;
-
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : "showcase_4_op_een_rij.client";
-
-if (!certificateName) {
-    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
-    process.exit(-1);
-}
-
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [viteReact()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    server: {
-        
-        proxy: {
-            // '/api': {
-            //     target: `${process.env.VITE_BACKEND_URL}/`,
-            //     secure: false,
-            //     changeOrigin: true,
-            //     rewrite: (path) => path.replace(/^\/api/, ''),
-            // },
-
-                '/hub': {
-                    target: `${process.env.VITE_BACKEND_URL}/hub`,
-                    secure: false,
-                    changeOrigin: true,
-                },
-               
-           
-        },
-        port: 3000,
-        host: '127.0.0.1',
-    }
+  plugins: [react()],
 })
