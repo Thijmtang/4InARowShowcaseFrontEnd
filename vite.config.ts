11d634e10ad034af0a977b99d56eb20cdb3c5da3
Thijmen Tang <thijmtang2003@gmail.com>
2025-09-14 21:04:01 +0200

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/showcase/",
   server: {
     watch: {
       usePolling: true,
     },
     host: true,
     strictPort: true,
-    port: parseInt(process.env.VITE_PORT),
+    port: Number(process.env.VITE_PORT),
   },
 });
